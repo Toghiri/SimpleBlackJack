5b3cd39ec742c657e15f2937a5eb275ca28e7c24
Toghiri <pietrovaccariinfo@gmail.com>
2023-02-10 14:48:22 +0100

    1.2 (DEALER'S HAND FIXED AND QOL CHANGES)

--- BlackJackT/Program.cs
+++ BlackJackT/Program.cs
@@ -3,25 +3,25 @@
 namespace BlackJackT
 {
     internal class Program
-    {   
+    {
         // La funzione "Main" gestisce il punteggio e la reiterazione della funzione "Sel".
         static void Main(string[] args)
         {
             int won = 0;
             int lost = 0;
             int tie = 0;
-            int res = 4;
-            Console.WriteLine("\nWELCOME TO THE BLACKJACK TABLE\n");
-            while (res == 4)
+            int res = 0;
+            Console.WriteLine("\nWELCOME TO THE BLACKJACK TABLE");
+            while (res == 0)
             {
                 res = Sel(res, won, lost, tie);
                 if (res == 1) { lost++; }
                 if (res == 2) { won++; }
                 if (res == 3) { tie++; }
-                res = 4;
+                res = 0;
             }
         }
-        // La funzione "Sel" gestisce la selezione tra: "Play" , "Punteggio" , "Fine".
+        // La funzione "Sel" gestisce la selezione tra: "Play" , "Score" , "Exit".
         static int Sel(int sres, int swon, int slost, int stie)
         {
             Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -29,20 +29,20 @@ namespace BlackJackT
             Console.ForegroundColor = ConsoleColor.Red;
             var input = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.White;
-        // Il comando "Play" fa iterare la funzione "Gioco" e resituisce il risultato.
-            while (input == "PLAY" || input == "play")
+            // Il comando "Play" fa iterare la funzione "Game" e resituisce il risultato.
+            while (input == "PLAY" || input == "play" || input == "1")
             {
                 sres = Game(sres);
                 input = "";
             }
-        // Il comando "Score" stampa il una stringa contenete il punteggio.
-            while (input == "SCORE" || input == "score")
+            // Il comando "Score" stampa il una stringa contenete il punteggio.
+            while (input == "SCORE" || input == "score" || input == "2")
             {
                 Console.WriteLine($"\nWON:   {swon}\nLOST:  {slost}\nTIED:  {stie}");
                 input = "";
             }
-        // Il comando "Exit" stampa: "THANK YOU FOR THE GAME!" e termina il programma.
-            while (input == "EXIT" || input == "exit")
+            // Il comando "Exit" stampa il punteggio e termina il programma.
+            while (input == "EXIT" || input == "exit" || input == "3")
             {
                 Console.WriteLine("\nTHANK YOU FOR THE GAME!");
                 Environment.Exit(0);
@@ -66,7 +66,7 @@ namespace BlackJackT
                 m.Add(rd.Next(1, 14));
             }
             Console.WriteLine("\nDEALER CARDS:\n");
-            Console.WriteLine($"{FormatHand(m)}    TOT VALUE: {HandScore(m)}");
+            Console.WriteLine($"{DealerHand(m)}");
             while (HandScore(c) < 22)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -74,7 +74,7 @@ namespace BlackJackT
                 Console.ForegroundColor = ConsoleColor.Red;
                 var command = Console.ReadLine();
                 Console.ForegroundColor = ConsoleColor.White;
-                if (command == "HIT" || command == "hit")
+                if (command == "HIT" || command == "hit" || command == "1")
                 {
                     c.Add(rd.Next(1, 14));
                     Console.WriteLine("\nYOUR CARDS:\n");
@@ -143,6 +143,12 @@ namespace BlackJackT
         {
             const string symbols = "A23456789XJQK";
             return string.Join(" | ", l.Select(x => symbols[x - 1]));
+        }
+        static string DealerHand(List<int> l)
+        {
+            const string symbols = "A23456789XJQK";
+            var hand = l.Take(1).Select(x => symbols[x - 1]).Union(l.Skip(1).Select(x => '*'));
+            return string.Join(" | ", hand);
         }
         // La funzione "HandScore" restituisce la somma dei valori della Lista.
         static int HandScore(List<int> l)
